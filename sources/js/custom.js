ea3e2397c85e0735f24d3e4b30126c5d3bbd6f8e
Eyetu Kingsley <eyetukingsley330@gmail.com>
2022-06-03 08:59:51 +0100

    I am done with the dashboard page of the site. Next up is to make it responsive.

--- sources/js/custom.js
+++ sources/js/custom.js
@@ -21,4 +21,24 @@ const swiper = new Swiper('.news-swiper', {
     slidesPerView: 'auto',
     initialSlide: 0,
 });
+
   
+const tabs = document.querySelector(".wrapper");
+const tabButton = document.querySelectorAll(".tab-button");
+const contents = document.querySelectorAll(".content");
+
+tabs.onclick = e => {
+  const id = e.target.dataset.id;
+  if (id) {
+    tabButton.forEach(btn => {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+
+    contents.forEach(content => {
+      content.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+}
