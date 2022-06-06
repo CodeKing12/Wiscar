c1a33fbc8ba70a18311fcad8a119bde9130ed8a0
Eyetu Kingsley <eyetukingsley330@gmail.com>
2022-06-06 09:58:53 +0100

    I am done with the payment details page and with making it responsive. Next up is to configure the masking for the cc number field.

--- sources/js/custom.js
+++ sources/js/custom.js
@@ -46,24 +46,26 @@ const swiper = new Swiper('.news-swiper', {
     // effect: 'fade'
 });
 
-  
+
 const tabs = document.querySelector(".wrapper");
 const tabButton = document.querySelectorAll(".tab-button");
 const contents = document.querySelectorAll(".content");
 
-tabs.onclick = e => {
-  const id = e.target.dataset.id;
-  if (id) {
-    tabButton.forEach(btn => {
-      btn.classList.remove("active");
-    });
-    e.target.classList.add("active");
-
-    contents.forEach(content => {
-      content.classList.remove("active");
-    });
-    const element = document.getElementById(id);
-    element.classList.add("active");
+if (tabs !== null) {
+  tabs.onclick = e => {
+    const id = e.target.dataset.id;
+    if (id) {
+      tabButton.forEach(btn => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+  
+      contents.forEach(content => {
+        content.classList.remove("active");
+      });
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
   }
 }
 
@@ -104,3 +106,5 @@ function decrement(elClass) {
   }
   element.value = value;
 }
+
+$("#cardNumber").mask("9999  -  9999  -  9999  -  9999");
